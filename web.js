f7b1ee0d13b4098bb7c937b884bc239b84ecb08e
Luca Agostini <luca.agostini@yourvoice.com>
2018-09-19 00:35:55 +0200

    Preparation for project #4

--- web.js
+++ web.js
@@ -3,20 +3,25 @@ const bodyParser = require("body-parser");
 const http = require("http");
 const chaintCTRL = require("./chainController");
 const Blockchain = require("./simpleChain");
-
+const moment = require("moment")
 const LISTENPORT = process.env.PORT || 8000;
 const blockchain = new Blockchain() 
+let userReqs =[]
 app.use(bodyParser.json({ limit: "50mb" }));
 
-//Decorate all requests with blockchain
+//Decorate all requests with blockchain and userRequests Buffer
 app.use((req, res, next) => {
   req.chain = blockchain;
+  req.userReqs = userReqs
   next()
 });
 
 app.route("/block/:height").get(chaintCTRL.getBlock);
 app.route("/block").post(chaintCTRL.setBlock);
-
+app.route("/yo").get((req, res) => {
+  req.userReqs.push("Uella "+ moment().format())
+res.json(req.userReqs)
+})
 http
   .createServer(app)
   .listen(LISTENPORT, () => console.log("Server listening on", LISTENPORT));

cd04bd9037a95c333b028c36d31a5535f196cc46
Luca Agostini <parmesanking@gmail.com>
2018-09-21 19:30:16 +0200

    Done message signing

--- chainController.js
+++ chainController.js
@@ -1,4 +1,9 @@
-const Block = require("./Block")
+const Block = require("./Block");
+const WAValidator = require("wallet-address-validator");
+const moment = require("moment");
+const bitcoinMessage = require("bitcoinjs-message");
+
+const VALIDATION_WINDOW_TIME = 300;
 
 const getBlock = (req, res) => {
   let height = req.params.height;
@@ -13,7 +18,7 @@ const getBlock = (req, res) => {
 
 const setBlock = (req, res) => {
   let data = req.body.body;
-  if (!data || !(typeof data === 'string' || data instanceof String)) {
+  if (!data || !(typeof data === "string" || data instanceof String)) {
     return res.status(500).send("Invalid data passed.");
   }
   req.chain.addBlock(new Block(data)).then(result => {
@@ -25,5 +30,109 @@ const setBlock = (req, res) => {
     }
   });
 };
+const requestValidation = (req, res) => {
+  let blockchainID = req.body.address;
+  if (
+    !blockchainID ||
+    !(typeof blockchainID === "string" || blockchainID instanceof String) ||
+    !WAValidator.validate(blockchainID, "BTC")
+  ) {
+    return res
+      .status(500)
+      .send("Invalid data passed, please send a valid wallet address");
+  }
 
-module.exports = { getBlock, setBlock };
+  //check if same wallet already has a request
+  let tStamp = moment().unix();
+  let validationRequest = {
+    address: blockchainID,
+    requestTimeStamp: tStamp,
+    message: `${blockchainID}:${tStamp}:starRegistry`,
+    validationWindow: getValidationWindow(tStamp)
+  };
+
+  let pending = req.app.locals.userReqs.filter(
+    uReq => uReq.address === blockchainID
+  )[0];
+  if (pending) {
+    validationRequest = { ...pending };
+    validationRequest.validationWindow = getValidationWindow(
+      pending.requestTimeStamp
+    );
+  } else {
+    req.app.locals.userReqs.push(validationRequest);
+  }
+  res.send(validationRequest);
+};
+
+const validateSignature = (req, res) => {
+  let blockchainID = req.body.address;
+  let signature = req.body.signature;
+  if (
+    !blockchainID ||
+    !(typeof blockchainID === "string" || blockchainID instanceof String) ||
+    !WAValidator.validate(blockchainID, "BTC")
+  ) {
+    return res
+      .status(500)
+      .send("Invalid data passed, please send a valid wallet address");
+  }
+  if (
+    !signature ||
+    !(typeof signature === "string" || signature instanceof String)
+  ) {
+    return res
+      .status(500)
+      .send("Invalid data passed, please send a valid signature");
+  }
+
+  //Prepare the answer
+  let result = {
+    registerStar: false,
+    status: {
+      address: null,
+      requestTimeStamp: null,
+      message: null,
+      validationWindow: 0,
+      messageSignature: 'invalid'
+    }
+  };
+  //Look for existing user request
+
+  let userRequest = req.app.locals.userReqs.filter(
+    uReq => uReq.address === blockchainID
+  )[0];
+  if (userRequest) {
+    let isValidSignature = bitcoinMessage.verify(
+      userRequest.message,
+      blockchainID,
+      signature
+    );
+    result.registerStar = isValidSignature;
+    result.status.address = userRequest.address;
+    result.status.requestTimeStamp = userRequest.requestTimeStamp;
+    result.status.message = userRequest.message;
+    result.status.validationWindow = getValidationWindow(
+      userRequest.requestTimeStamp
+    );
+    result.status.messageSignature = isValidSignature ? 'valid':'invalid';
+  } else {
+    result.status.address = blockchainID;
+    result.status.message = "User request not found or expired";
+    delete result.status.validationWindow;
+    delete result.status.requestTimeStamp;
+  }
+  res.send(result);
+};
+const getValidationWindow = from => {
+  return moment
+    .unix(parseInt(from) + VALIDATION_WINDOW_TIME)
+    .diff(moment(), "seconds");
+};
+module.exports = {
+  getBlock,
+  setBlock,
+  requestValidation,
+  getValidationWindow,
+  validateSignature
+};
--- web.js
+++ web.js
@@ -3,25 +3,35 @@ const bodyParser = require("body-parser");
 const http = require("http");
 const chaintCTRL = require("./chainController");
 const Blockchain = require("./simpleChain");
-const moment = require("moment")
+const moment = require("moment");
 const LISTENPORT = process.env.PORT || 8000;
-const blockchain = new Blockchain() 
-let userReqs =[]
+const blockchain = new Blockchain();
+
 app.use(bodyParser.json({ limit: "50mb" }));
+//Clean expired validation request
+app.use((req, res, next) => {
+  app.locals.userReqs = (app.locals.userReqs ||[]).filter(
+    uReq => true //chaintCTRL.getValidationWindow(uReq.requestTimeStamp) > 0
+  );
+  next();
+});
 
 //Decorate all requests with blockchain and userRequests Buffer
 app.use((req, res, next) => {
   req.chain = blockchain;
-  req.userReqs = userReqs
-  next()
+  next();
 });
+
+
 
 app.route("/block/:height").get(chaintCTRL.getBlock);
 app.route("/block").post(chaintCTRL.setBlock);
+app.route("/requestValidation").post(chaintCTRL.requestValidation);
+app.route("/message-signature/validate").post(chaintCTRL.validateSignature)
+
 app.route("/yo").get((req, res) => {
-  req.userReqs.push("Uella "+ moment().format())
-res.json(req.userReqs)
-})
+  res.json(req.userReqs);
+});
 http
   .createServer(app)
   .listen(LISTENPORT, () => console.log("Server listening on", LISTENPORT));
